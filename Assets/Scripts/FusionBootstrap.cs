cce76b884a8c996edc5c5ed499862f6cafa8e078
jinjooyoung <jjy26297449@gmail.com>
2026-04-15 11:33:46 +0900

    [Feat] 6강까지 프로젝트 받아오기, Photon 세팅

---
+++ Assets/Scripts/FusionBootstrap.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+
+public class FusionBootstrap : MonoBehaviour , INetworkRunnerCallbacks
+{
+    [Header("Session")]
+    [SerializeField] private string sessionName = "Room_01";
+
+    [Header("Player")]
+    [SerializeField] private NetworkPrefabRef playerPrefab;                             //네트워크에 등록된 프리팹
+    [SerializeField] private Transform[] spawnPoints;                                   //스폰 위치 설정 
+
+    private Dictionary<PlayerRef, NetworkObject> playerObjects = new();
+
+    private NetworkRunner runner;
+
+    public struct NetworkInputData : INetworkInput
+    {
+        public Vector2 move;
+        public NetworkButtons buttons;
+    }
+
+    public enum InputButton
+    {
+        Fire = 0,
+        Jump = 1
+    }
+
+
+    public void StartHost() => _ = StartGame(GameMode.Host);
+    public void StartClinet() => _ = StartGame(GameMode.Client);
+
+    private Vector3 GetSpawnPosition(PlayerRef player)
+    {
+        if (spawnPoints != null && spawnPoints.Length > 0)
+        {
+            int index = player.RawEncoded % spawnPoints.Length;
+            return spawnPoints[index].position;
+        }
+
+        return new Vector3(player.RawEncoded * 2, 1, 0);                //RawEncoded (바이트(byte)) 형태로 변환 (직렬화) 중간단계
+    }
+
+    private async Task StartGame(GameMode mode)
+    {
+        if (runner != null) return;
+
+        runner = gameObject.AddComponent<NetworkRunner>();
+        runner.ProvideInput = true;
+
+        runner.AddCallbacks(this);
+
+        var SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>();
+
+        var result = await runner.StartGame(new StartGameArgs
+        {
+            GameMode = mode,
+            SessionName = sessionName,
+            SceneManager = SceneManager 
+        });
+
+        if (result.Ok)
+            Debug.Log($"[Fusion] StartGame OK - {mode} / {sessionName}");
+        else
+            Debug.LogError($"[Fusion] StartGame FAILED - {result.ShutdownReason}");
+    }
+
+  
+
+    // --------------------- 콜백 (필수/미사용은 빈 구현) -------------------
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) 
+    {
+        Debug.Log($"플레이어 입장 : {player}");
+
+        if (!runner.IsServer)
+            return;
+
+        Vector3 spawnPos = GetSpawnPosition(player);
+
+        var obj = runner.Spawn(
+            playerPrefab,
+            spawnPos,
+            Quaternion.identity,
+            player
+        );
+
+        playerObjects[player] = obj;
+
+    }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) 
+    {
+        if (!runner.IsServer) return;
+
+        if (playerObjects.TryGetValue(player, out var obj))
+        {
+            runner.Despawn(obj);
+            playerObjects.Remove(player);
+        }
+
+        Debug.Log($"플레이어 제거됨 : {player}");
+    
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        NetworkInputData data = new NetworkInputData();
+
+        data.move = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+
+        var buttons = new NetworkButtons();                                         //네트워크 버튼 생성 
+        buttons.Set((int)InputButton.Fire, Input.GetMouseButton(0));                //마우스 버튼
+        buttons.Set((int)InputButton.Jump, Input.GetKey(KeyCode.Space));            //점프 버튼
+
+        data.buttons = buttons;
+
+        input.Set(data);
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player , NetworkInput input) { }
+
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner , NetDisconnectReason reason)    
+    {
+        Debug.Log($"[Fusion] Disconnected : {reason}");
+    }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason reason)
+    {
+        Debug.Log($"[Fusion] Shutdown : {reason}");
+        this.runner = null;
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request , byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+
+    //------------------------------------------------------------------------------------------------------------
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+}
---
+++ Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,226 @@
+using Fusion;
+using UnityEngine;
+
+public class SimplePlayer : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotateSpeed = 10f;
+
+    [Header("Bullet")]
+    [SerializeField] private NetworkPrefabRef bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+    [SerializeField] private float fireDistance = 20f;
+    [SerializeField] private LayerMask hitMask;
+
+    [Networked] private TickTimer FireCooldown {  get; set; }
+    [SerializeField] private float fireInterval = 0.2f;
+
+    [SerializeField] private Animator animator;
+    [Networked] private float MoveSpeedNet { get; set; }
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 6f;
+    [SerializeField] private float gravity = -20f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Networked] private int JumpTick { get; set; }
+    private int _lastRenderedJumpTeck = -1;
+
+    [Networked] private float VerticalVelocity { get; set; }
+    [Networked] private NetworkBool IsGroundedNet { get; set; }
+    [Networked] private NetworkBool JumpTriggeredNet { get; set; }
+    [Networked] private NetworkButtons PreviousButtons {  get; set; }
+
+    private int _lastJumpVisualTick = -1;
+
+    [SerializeField] private GameObject cameraRoot;
+    private Transform cameraTransform;
+
+    public override void Spawned()
+    {
+        if (cameraRoot == null) return;
+
+        bool isMine = Object.HasInputAuthority;
+        cameraRoot.SetActive(isMine);
+
+        if (isMine)
+        {
+            Camera cam = cameraRoot.GetComponentInChildren<Camera>(true);
+            if (cam != null)
+            {
+                cameraTransform = cam.transform;
+            }
+        }            
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput<FusionBootstrap.NetworkInputData>(out var inputData))
+        {
+            Vector3 move;
+
+            if (Object.HasInputAuthority && cameraTransform != null)
+            {
+                Vector3 forward = cameraTransform.forward;
+                forward.y = 0f;
+                forward.Normalize();
+
+                Vector3 right = Vector3.Cross(forward, Vector3.up).normalized;
+
+                move = forward * inputData.move.y - right * inputData.move.x;
+            }
+            else
+            {
+                move = new Vector3(inputData.move.x, 0.0f, inputData.move.y);
+
+            }
+
+            if (move.sqrMagnitude > 1f)
+                move.Normalize();
+
+            MoveSpeedNet = move.magnitude;
+
+            //바닥 체크
+            bool grounded = Physics.CheckSphere(
+                groundCheck != null ? groundCheck.position : transform.position + Vector3.down * 0.9f,
+                groundCheckRadius,
+                groundMask
+            );
+
+            IsGroundedNet = grounded;
+
+            //바닥에 붙어있을때 처리 
+            if(grounded && VerticalVelocity < 0.0f)
+            {
+                VerticalVelocity = 0f;
+            }
+
+            //점프 
+            if (grounded && inputData.buttons.WasPressed(PreviousButtons, (int) FusionBootstrap.InputButton.Jump))
+            {
+                VerticalVelocity = jumpForce;
+                IsGroundedNet = false;
+                grounded = false;
+                JumpTick = Runner.Tick;
+            }
+
+            //중력 
+            VerticalVelocity += gravity * Runner.DeltaTime;
+
+            //이동 분리 
+            Vector3 horizontalMove = new Vector3(move.x * moveSpeed , 0f, move.z * moveSpeed );
+            transform.position += horizontalMove * Runner.DeltaTime;
+
+            if(!(grounded && VerticalVelocity <= 0f))
+            {
+                Vector3 verticalMove = new Vector3(0f, VerticalVelocity, 0f);
+                transform.position += verticalMove * Runner.DeltaTime;
+            }
+
+            PreviousButtons = inputData.buttons;
+
+            
+
+            if (move.sqrMagnitude > 0.001f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    targetRotation,
+                    rotateSpeed * Runner.DeltaTime
+                );
+            }
+        }
+
+        //발사
+        if (inputData.buttons.IsSet((int)FusionBootstrap.InputButton.Fire))
+        {
+            if(FireCooldown.ExpiredOrNotRunning(Runner))
+            {
+                FireLagCompensated();
+                FireCooldown = TickTimer.CreateFromSeconds(Runner, fireInterval);
+            }
+           
+        }
+    }
+
+    private void Fire()
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        Vector3 spawnPos = firePoint != null
+            ? firePoint.position
+            : transform.position + transform.forward + Vector3.up * 0.5f;
+
+        Quaternion spawnRot = transform.rotation;
+
+        NetworkObject bulletObj = Runner.Spawn(
+            bulletPrefab,
+            spawnPos,
+            spawnRot,
+            Object.InputAuthority
+        );
+
+        SimpleBullet bullet = bulletObj.GetComponent<SimpleBullet>();
+        if (bullet != null)
+        {
+            bullet.Init(Object.InputAuthority);
+        }
+    }
+
+    private void FireLagCompensated()
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        Vector3 origin = firePoint != null ? firePoint.position : transform.position + Vector3.up * 0.5f;
+        Vector3 direction = transform.forward;
+
+        //Runner -> NetworkRunner -> 게임의 네트워크 전체를 관리하는 관리자
+        if (Runner.LagCompensation.Raycast(
+            origin,
+            direction,
+            fireDistance,
+            Object.InputAuthority,
+            out LagCompensatedHit hit,
+            hitMask
+        ))
+        {
+            Debug.Log($"LagComp Hit : {hit.Hitbox.name}");
+            RPC_PlayHitEffect(hit.Point, hit.Normal);
+            Hitbox hitbox = hit.Hitbox;
+            if (hitbox != null)
+            {
+                HealthTarget target = hitbox.GetComponentInParent<HealthTarget>(); 
+                if (target != null)
+                {
+                    target.TakeDamage(1);
+                }
+            }
+        }
+            
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_PlayHitEffect(Vector3 pos, Vector3 normal)
+    {
+        if (EffectManager.instance == null) return;
+        EffectManager.instance.PlayerWorldEffect(EffectManager.instance.HitEffect, pos, normal);
+    }
+
+    public override void Render()
+    {
+        if (animator == null) return;
+
+        animator.SetFloat("Speed", MoveSpeedNet);
+        animator.SetBool("Grounded", IsGroundedNet);
+        animator.SetBool("Jump", !IsGroundedNet && VerticalVelocity > 0.1f);
+        animator.SetBool("FreeFall", !IsGroundedNet && VerticalVelocity <= 0.1f);
+        animator.SetFloat("MotionSpeed", 3f);
+    }
+}
